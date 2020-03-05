6b6c516028e5e7572f39161a9b1b7d9f535f184f
Jake Lee <firstvirtue@gmail.com>
2020-03-05 22:08:10 +0900

    Add new routes

---
+++ pages/auth/login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>User</h1>
+    <p>User ID : {{id}}</p>
+  </div>
+</template> <script>
+export default {
+  asyncData({ params }) {
+    console.log(params);
+    return { id: params.id };
+  },
+  validate({ params }) {
+    // return /^\d+$/.test(params.id);
+    return params.id;
+  }
+};
+</script>
