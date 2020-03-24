97a488f086c61274f261cba79fd74a7328a71d41
Jake Lee <firstvirtue@gmail.com>
2020-03-25 00:36:06 +0900

    Add header module

--- assets/js/module/editor.js
+++ assets/js/module/editor.js
@@ -5,11 +5,20 @@ class Editor {
     if(process.browser) {
       const EditorJS = require('@editorjs/editorjs');
       const ImageTool = require('@editorjs/image');
+      const Header = require('@editorjs/header');
 
       this.editor = new EditorJS({
         holder: container,
         data: data,
         tools: {
+          header: {
+            class: Header,
+            config: {
+              placeholder: '소제목',
+              levels: [1, 2],
+              defaultLevel: 2
+            }
+          },
           image: {
             class: ImageTool,
             config: {
