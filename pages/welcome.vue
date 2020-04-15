eeb1b46e439acc6a3136bb691a9155ecbc2c59ce
Jake Lee <firstvirtue@gmail.com>
2020-04-15 20:53:27 +0900

    Add welcome layout base

--- pages/welcome.vue
+++ pages/welcome.vue
@@ -1,25 +1,42 @@
 <template>
   <main class="main">
-    <div class="intro" data-invert>
+    <div class="welcome" data-invert>
       <section class="section section--cover">
         <div class="wrapper wrapper--m-p">
-          <h1 class="intro__title">소개합니다</h1>
-
-          <!-- <img src="" alt="">
-          <video src=""></video> -->
-
-          <div class="cover-desc">
-            하나님을 경외하고<br>
-            사람을 사랑하는 교회
+          <div class="cover">
+            <h1 class="welcome__title">소개합니다</h1>
+            <div class="welcome__desc">
+              하나님을 경외하고<br>
+              사람을 사랑하는 교회
+            </div>
           </div>
         </div>
       </section>
 
-      <!-- <section class="section section--feature">
-
-      </section> -->
+      <section class="section section--feature">
+        <h2 class="a11y">교회 특징</h2>
+        <div class="wrapper wrapper--m-p">
+          <figure>
+            <video src="~/assets/video/prayfor.mp4" playsinline loop autoplay muted></video>
+            <!-- <ficaption>성도를 위한 담임목사 기도</ficaption> -->
+          </figure>
+          <figure>
+            <img src="~/assets/image/welcome/friends.jpeg" alt="">
+            <!-- <ficaption>서울숲 청년 교제</ficaption> -->
+          </figure>
+          <figure>
+            <img src="~/assets/image/welcome/bowling.jpeg" alt="">
+            <!-- <ficaption>주일 오후 청년 교제</ficaption> -->
+          </figure>
+          <figure>
+            <video src="~/assets/video/harmonica.mp4" playsinline loop autoplay muted></video>
+            <!-- <ficaption>하모니카 찬양단</ficaption> -->
+          </figure>
+        </div>
+      </section>
 
       <section class="section section--religious-worker">
+        <h2>섬기는 사람들</h2>
 
       </section>
 
@@ -74,7 +91,7 @@
 
       <section class="section section--location">
         <div class="wrapper wrapper--m-p">
-          <h2>교회 위치</h2>
+          <h2>오시는 길</h2>
           <div class="cont">
             04773<br>
             서울 성동구 뚝섬로 312<br>
@@ -93,11 +110,14 @@
   </main>
 </template>
 
-<style lang="scss" src="~/assets/scss/page/_intro.scss"></style>
+<style lang="scss" src="~/assets/scss/page/_welcome.scss"></style>
 
 <script>
 export default {
   mounted() {
+
+    // TODO: resize event
+    document.querySelector('.cover').style.height = window.innerHeight + 'px';
 
     var container = document.getElementById('map-container'); //지도를 담을 영역의 DOM 레퍼런스
     var options = { //지도를 생성할 때 필요한 기본 옵션
