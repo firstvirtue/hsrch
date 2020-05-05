1fb585a1abde15bb3dec46db8c7926dcad74fb4f
Jake Lee <firstvirtue@gmail.com>
2020-05-05 12:25:53 +0900

    Update map position

--- pages/welcome.vue
+++ pages/welcome.vue
@@ -162,7 +162,7 @@ export default {
 
     var container = document.getElementById('map-container'); //지도를 담을 영역의 DOM 레퍼런스
     var options = { //지도를 생성할 때 필요한 기본 옵션
-      center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+      center: new kakao.maps.LatLng(37.54124699815597, 127.04562849894883), //지도의 중심좌표.
       level: 3 //지도의 레벨(확대, 축소 정도)
     };
 
@@ -170,12 +170,12 @@ export default {
     // map.setDraggable(false);
     // map.setZoomable(false);
     var imageSrc = '/ri.png', // 마커이미지의 주소입니다
-      imageSize = new kakao.maps.Size(64, 69), // 마커이미지의 크기입니다
-      imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+      imageSize = new kakao.maps.Size(40, 40), // 마커이미지의 크기입니다
+      imageOption = {offset: new kakao.maps.Point(20, 20)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
 
     // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
     var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
-        markerPosition = new kakao.maps.LatLng(33.450701, 126.570667); // 마커가 표시될 위치입니다
+        markerPosition = new kakao.maps.LatLng(37.54124699815597, 127.04562849894883); // 마커가 표시될 위치입니다
 
     // 마커를 생성합니다
     var marker = new kakao.maps.Marker({
